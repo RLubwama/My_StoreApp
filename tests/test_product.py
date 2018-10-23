1615f95298ac9ba86f0f0b2007a7eb11f523e55c
Ronaldhi <ronaldblumberg23@gmail.com>
2018-10-23 16:12:00 +0300

    Adding tests

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,4 +1,19 @@
 import unittest
+import pytest
+import tests
 
 class Test(unittest.TestCase):
-    pass
+    products = {}
+    def product(self,product_id,name):
+        self.product_id = product_id
+        self.name = name
+        if name != products:
+            print ("product is out of stock")
+            # if condition:
+            #     pass
+            else:
+                if name = products:
+                    print("product in stock")
+                    return product
+
+
